50d13ba40dee879cb1aeb01426cdec8ebd497201
Antonio Miquelini <antoniomiquelini@gmail.com>
2022-01-23 09:01:34 -0300

    DepartmentList Controller e Department Service

--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -1,11 +1,21 @@
 package gui;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import application.Main;
+import gui.util.Alerts;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.MenuItem;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.VBox;
+import model.services.DepartmentService;
 
 public class MainViewController implements Initializable{
 	@FXML
@@ -25,12 +35,12 @@ public class MainViewController implements Initializable{
 	
 	@FXML
 	public void onMenuItemDepartmentAction () {
-		System.out.println("onMenuItemDepartmentAction");
+		loadView2("/gui/DepartmentList.fxml");	
 	}
 	
 	@FXML
 	public void onMenuItemAboutAction () {
-		System.out.println("onMenuItemAboutAction");
+		loadView("/gui/About.fxml");
 	}
 	
 	
@@ -39,6 +49,51 @@ public class MainViewController implements Initializable{
 		// TODO Auto-generated method stub
 		
 	}
+	
+	private synchronized void loadView(String absoluteName) {
+		try {
+			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
+			VBox newVBox = loader.load();
+			
+			Scene mainScene = Main.getMainScene();
+			VBox mainVBox = (VBox) ((ScrollPane) mainScene.getRoot()).getContent();
+			
+			Node mainMenu = mainVBox.getChildren().get(0);
+			mainVBox.getChildren().clear();
+			mainVBox.getChildren().add(mainMenu);
+			mainVBox.getChildren().addAll(newVBox.getChildren());	
+			
+			
+		}
+		catch (IOException e) {
+			Alerts.showAlert("IOExecption", "Error loader view", e.getMessage(), AlertType.ERROR);
+		}
+		
+	}
 
+	private synchronized void loadView2(String absoluteName) {
+		try {
+			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
+			VBox newVBox = loader.load();
+			
+			Scene mainScene = Main.getMainScene();
+			VBox mainVBox = (VBox) ((ScrollPane) mainScene.getRoot()).getContent();
+			
+			Node mainMenu = mainVBox.getChildren().get(0);
+			mainVBox.getChildren().clear();
+			mainVBox.getChildren().add(mainMenu);
+			mainVBox.getChildren().addAll(newVBox.getChildren());	
+			
+			DepartmentListController controller = loader.getController();
+			controller.setDepartmentService(new DepartmentService());
+			controller.updateTableView();
+			
+		}
+		catch (IOException e) {
+			Alerts.showAlert("IOExecption", "Error loader view", e.getMessage(), AlertType.ERROR);
+		}
+		
+	}
 
+	
 }
